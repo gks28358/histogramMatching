db4af9f1a8b3876ba0a6b47b87891648519439c7
Saurabh Savara <savaras20@students.ecu.edu>
2020-10-12 22:40:33 -0400

    Update Histogram.py

--- Histogram.py
+++ Histogram.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
